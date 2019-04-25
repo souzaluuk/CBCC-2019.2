4ab59b4ed955c1de1322376e13ad61db75d90025
Lucas Souza <lucassouzaufpa@gmail.com>
2019-04-25 11:29:00 -0300

    Adiciona funçao de desenho de linha a interface - computaçao grafica

--- computacao-grafica/codes/app.py
+++ computacao-grafica/codes/app.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 from tkinter.colorchooser import askcolor
+from algoritmos import bresenham
 #
 #No manjaro, necessário usar:
 # `pacman -S tk`
 #
 class App(tk.Tk):
+    w_bt = 6
+    frame_buffer_aux = []
     def __init__(self, escala=10, largura=600, altura=600, titulo='CG-2019.2'):
         super().__init__()
         # MODOS = LIVRE, LINHA, CIRCULO, CURVA, PREE_REC, PREE_SCAN
@@ -14,16 +17,54 @@ class App(tk.Tk):
         self.escala = escala
         self.ferramentas = None
         self.canvas = None
-        self.frame_buffer = []
         self.cor = 'black'
-        self.modos = None
+        self.frame_buffer = None
+        self.modo = 'LIVRE'
+
+    def limpa_buffer(self):
+        # inicializa frame buffer
+        self.frame_buffer = [['white' for x in range(self.largura//self.escala)] for y in range(self.altura//self.escala)]
+        self.frame_buffer_aux = []
+        self.pinta_buffer()
 
     def show(self):
         self.canvas = self.cria_canvas() # cria canvas na tela do app
+        self.limpa_buffer()
+        self.pinta_buffer() # após a criação do canvas
+        self.cria_eventos() # criação dos eventos de botões e cliques no canvas
         self.ferramentas = self.cria_ferramentas() # cria caixa de ferramentas na tela do app
         self.geometry("%dx%d+0+0" % (self.largura+int(self.largura*0.13), self.altura)) # dimensões totais da 
         self.resizable(False,False)
         self.mainloop()
+
+    def cria_eventos(self):
+        modo = self.modo
+        f_event = None
+        self.frame_buffer_aux = []
+        if modo=='LIVRE':
+            def f(event):
+                x,y = self.xyscala(event.x,event.y) # converte de acordo com escala
+                self.add_frame_buffer(x,y,self.cor)
+                self.pinta_buffer()
+            f_event = f
+        elif modo=='LINHA':
+            # pintando linha a cada novo clique ( para finalizar uma linha)
+            # para finalização de uma linha deve-se mudar o modo de pintura, ou clicar em linha novamente
+            def f(event):
+                x,y = self.xyscala(event.x,event.y) # converte de acordo com escala
+                if self.frame_buffer_aux: # caso haja algo no buffer auxiliar
+                    x0,y0 = self.frame_buffer_aux[-1]
+                    self.frame_buffer_aux = bresenham((x0,y0),(x,y))
+                    for x,y in self.frame_buffer_aux:
+                        self.add_frame_buffer(x,y,self.cor)
+                else: # se buffer auxiliar vazio
+                    self.frame_buffer_aux.append((x,y))
+                    self.add_frame_buffer(x,y,self.cor)
+                self.pinta_buffer()
+            f_event = f
+        # substitui evento modo de pintura atual
+        self.canvas.unbind('<Button-1>')
+        self.canvas.bind('<Button-1>',f_event)
 
     def cria_ferramentas(self):
         frame_ferramenta = tk.Frame(self)
@@ -38,14 +79,9 @@ class App(tk.Tk):
             label_ponto['text'] = '('+x+','+y+')'
         def leave_mouse(event):
             label_ponto['text']='(00,00)'
-        def press_mouse(event): 
-            x,y = self.xyscala(event.x,event.y)
-            self.frame_buffer.append((x,y,self.cor))
-            #self.pinta_pixel(x,y,self.cor)
-            self.pinta_buffer()
         self.canvas.bind('<Motion>',motion_mouse) # liga função ao evento
         self.canvas.bind('<Leave>',leave_mouse)
-        self.canvas.bind('<Button-1>',press_mouse)
+        #self.canvas.bind('<Button-1>',self.clica_canvas)
         # instância do label que muda a cor
         label_cor = tk.Label(frame_ferramenta,text='      ',bg=self.cor)
         # evento para mudança de cor
@@ -56,62 +92,56 @@ class App(tk.Tk):
                 label_cor.configure(bg=self.cor)
         label_cor.bind('<Button-1>',muda_cor)
         label_cor.pack()
-        # limpar tela
-        def limpar_tela(event):
-            self.frame_buffer = []
-            self.canvas.delete('all')
-            for x in range(0,self.largura,self.escala):
-                self.canvas.create_line(x,0,x,self.altura)
-            for y in range(0,self.altura,self.escala):
-                self.canvas.create_line(0,y,self.largura,y)
 
-        limpar = tk.Button(frame_ferramenta,text='Limpar',width=6)
-        limpar.bind('<Button-1>',limpar_tela)
-        limpar.pack()
-
-        self.cria_botoes_modos(frame_ferramenta)
+        botoes = self.cria_botoes_modos(frame_ferramenta)
         return {
             'ponto':label_ponto,
-            'cor':label_cor
+            'cor':label_cor,
+            'botoes':botoes
         }
-    
+
+    def muda_opcao(self,modo):
+        self.modo = modo
+        self.cria_eventos()
+
     def cria_botoes_modos(self,pai):
-        w = 6
+        w_bt = self.w_bt
         botoes = [
-            tk.Button(pai,text='Linha',command=None,width=w),
-            tk.Button(pai,text='Círculo',command=None,width=w),
-            tk.Button(pai,text='Bezier',command=None,width=w),
-            tk.Button(pai,text='Pre. Rec',command=None,width=w),
-            tk.Button(pai,text='Pre. Scan',command=None,width=w),
+            tk.Button(pai,text='Limpar',command=self.limpa_buffer,width=w_bt),
+            tk.Button(pai,text='Livre',command=lambda: self.muda_opcao('LIVRE'),width=w_bt),
+            tk.Button(pai,text='Linha',command=lambda: self.muda_opcao('LINHA'),width=w_bt),
+            tk.Button(pai,text='Círculo',command=lambda: self.muda_opcao('circulo'),width=w_bt),
+            tk.Button(pai,text='Bezier',command=lambda: self.muda_opcao('bezier'),width=w_bt),
+            tk.Button(pai,text='Pre. Rec',command=lambda: self.muda_opcao('scanline'),width=w_bt),
+            tk.Button(pai,text='Pre. Scan',command=lambda: self.muda_opcao('recursivo'),width=w_bt),
         ]
         for botao in botoes:
             botao.pack()
         return botoes
     def cria_canvas(self):
         # guarda parâmetros para criação do canvas
-        escala = self.escala
         largura = self.largura
         altura = self.altura
         # instância do canvas
-        canvas = tk.Canvas(self,width=largura,height=altura,bg='white')
+        canvas = tk.Canvas(self,width=largura,height=altura)
         canvas.grid(row=0)
-        # pintura da grade de pixels
-        for x in range(0,largura,escala):
-            canvas.create_line(x,0,x,altura)
-        for y in range(0,altura,self.escala):
-            canvas.create_line(0,y,largura,y)
         return canvas
-    
-    def pinta_pixel(self,x,y,cor):
-        x *= self.escala
-        y *= self.escala
-        xfim = x+self.escala
-        yfim = y+self.escala
-        self.canvas.create_rectangle(x,y,xfim,yfim,fill=cor)
+
+    def add_frame_buffer(self,x,y,cor):
+        self.frame_buffer[x][y] = cor
+
     def pinta_buffer(self):
-        for pixel in self.frame_buffer:
-            x,y,cor = pixel
-            self.pinta_pixel(x,y,cor)
+        escala = self.escala # cópia local
+        for i_linha in range(len(self.frame_buffer)):
+            for i_coluna in range(len(self.frame_buffer[i_linha])):
+                cor = self.frame_buffer[i_linha][i_coluna]
+                x = i_linha*escala
+                y = i_coluna*escala
+                xfim = x+escala
+                yfim = y+escala
+                self.canvas.create_rectangle(
+                    x+1,y+1,xfim-1,yfim-1,fill=cor,outline=cor
+                )
     
     def xyscala(self,x,y):
         x //= self.escala
